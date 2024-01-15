f481e13bc409c5dc4ed1795c4ed788ca4dcdbd37
XyzCoder <106374961+YounisXyz@users.noreply.github.com>
2024-01-15 21:49:09 +0500

    Update Don.py

--- Don.py
+++ Don.py
@@ -2,6 +2,6 @@ import os,platform
 os.system('git pull')
 xyz=platform.architecture()[0]
 if xyz=="32bit":
-    print("\033[1;97m[\033[1;93m This Tool is only For 64bit Devices, Your Phone is 32bit, Please try Another Device or Wait for Some ill update this for 32bit users. \n\t\033[1;92m Thanks For Joining Younis John \033[1;97m ] 
+    print("\033[1;97m[\033[1;93m This Tool is only For 64bit Devices, Your Phone is 32bit, Please try Another Device or Wait for Some ill update this for 32bit users. \n\t\033[1;92m Thanks For Joining Younis John \033[1;97m ]") 
 elif xyz=="64bit":
     __import__("YOUNISXD")
